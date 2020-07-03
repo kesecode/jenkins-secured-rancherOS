b30b5a0caaebc1ee37904ab8e60154923cae02ae
dweppler <contact@david-weppler.de>
2020-07-03 17:15:23 +0200

    added docker volume create

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker volume create jenkins
 
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
@@ -9,7 +10,7 @@ domains=(jenkins.david-weppler.de)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="contact@david-weppler.de" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
